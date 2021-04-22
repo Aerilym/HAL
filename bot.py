378fe9a1027c45d637cb3f4cba3449b67b66e0e8
Aerilym <aerilymbiz@gmail.com>
2021-04-23 09:56:12 +1000

    update

--- bot.py
+++ bot.py
@@ -3,24 +3,27 @@ import os
 import discord
 from dotenv import load_dotenv
 
+from discord.ext import commands
+
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv('TOKEN_DISCORD')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='!')
 
 @client.event
 async def on_ready():
     for guild in client.guilds:
-        if guild.name == GUILD:
-            break
 
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})\n'
-    )
+        print(
+            f'{client.user} is connected to the following guild:\n'
+            f'{guild.name}(id: {guild.id})\n'
+        )
 
-    members = '\n - '.join([member.name for member in guild.members])
-    print(f'Guild Members:\n - {members}')
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
 
 client.run(TOKEN)
